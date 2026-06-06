c0269721d0e7346f40eabf4979bfb5f2d541d4bc
Rodi de Boer <rodi@procurios.nl>
2026-06-06 21:39:56 +0200

    Add info after hours of finding the cause of no test db

--- docker/init-test-db.sh
+++ docker/init-test-db.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+#Make sure this script has execute rights (chmod +x)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
   -c "CREATE DATABASE \"$TEST_DB_NAME\";"
